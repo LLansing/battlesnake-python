736c9092531d2b3eaf1db6b9e7569e801e0b989e
LLansing <35748909+LLansing@users.noreply.github.com>
2018-03-04 22:56:04 -0800

    set direction returns random else

--- app/main.py
+++ app/main.py
@@ -111,8 +111,7 @@ def check_move(ourx, oury, data):
     if ourx >= board_width or ourx < 0 or oury >= board_height or oury < 0:
         return 0
     for ourbod_seg in ourbody:
-        if ourbod_seg.get("x") == ourx:
-            return 0
+        return 0
     
     #for snek in data.get("snakes").get("data"):
     #    for bod_seg in snek.get("body").get("data"):
