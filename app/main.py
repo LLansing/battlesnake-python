d693fba05739a5555ec069ec21697766c3b45cf1
LLansing <35748909+LLansing@users.noreply.github.com>
2018-03-05 13:05:47 -0800

    hunger at 60, no while loop in check_move

--- app/main.py
+++ app/main.py
@@ -95,7 +95,7 @@ def set_direction(x_diff, y_diff, hx, hy, data):
             return 'up'
         if check_move(hx, hy + 1, data):
             return 'down'
-    return 'down'
+    return random.choice(['up', 'down', 'left', 'right'])
     
 def check_move(ourx, oury, data):
     
@@ -118,8 +118,8 @@ def check_move(ourx, oury, data):
     #        if bod_seg.get('x') == ourx and bod_seg.get('y') == oury:
     #            return 0
     for s in data['snakes']['data']:
-        #if (abs(s['body']['data'][0]['x'] - ourx) + abs(s['body']['data'][0]['y'] - oury)) == 1:
-        #    return 0
+        if (abs(s['body']['data'][0]['x'] - ourx) + abs(s['body']['data'][0]['y'] - oury)) == 1:
+            return 0
         for sb in s['body']['data']:
             if sb['x'] == ourx and sb['y'] == oury:
                 return 0
