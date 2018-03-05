7953a6391ec8b501ced4f6f21fd2eb34a6e3b290
LLansing <35748909+LLansing@users.noreply.github.com>
2018-03-05 12:43:51 -0800

    hunger at 60, no while loop in check_move

--- app/main.py
+++ app/main.py
@@ -119,7 +119,7 @@ def check_move(ourx, oury, data):
     #            return 0
     for s in data['snakes']['data']:
         if abs(s['body']['data'][0]['x'] - ourx) +  abs(s['body']['data'][0]['y'] - oury) == 1:
-                return 0
+            return 0
         for sb in s['body']['data']:
             if sb['x'] == ourx and sb['y'] == oury:
                 return 0
