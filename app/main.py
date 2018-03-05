79d73664641547bf0f3590c16b1ecd2f14ade6ce
LLansing <35748909+LLansing@users.noreply.github.com>
2018-03-05 00:02:48 -0800

    set direction returns random else

--- app/main.py
+++ app/main.py
@@ -104,16 +104,15 @@ def set_direction(x_diff, y_diff, hx, hy, data):
     
 def check_move(ourx, oury, data):
     
-    
     board_width = data.get('width')
     board_height = data.get('height')
     ourbody = data.get("you").get("body").get("data")
     if ourx >= board_width or ourx < 0 or oury >= board_height or oury < 0:
         return 0
-    for ourbod_seg in ourbody:
-        if ourbod_seg.get("x") == ourx and ourbod_seg.get("y") == oury:
-            return 0
-    
+    for seg in data.get("you").get("body").get("data"):
+        #if seg.get("x") == ourx and seg.get("y") == oury:
+        #    return 0
+        print "hello"
     #for snek in data.get("snakes").get("data"):
     #    for bod_seg in snek.get("body").get("data"):
     #        if bod_seg.get("x") == ourx and bod_seg.get("y") == oury:
